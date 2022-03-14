9cdb1564cc357835dc1b44ab9a33bfa05a97faf4
弘博 <56869106+xxmy7@users.noreply.github.com>
2022-03-14 09:06:58 +0800

    first init

---
+++ src/main/java/com/neeewbeeee/roamerserver/RoamerServerApplication.java
@@ -0,0 +1,13 @@
+package com.neeewbeeee.roamerserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoamerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoamerServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neeewbeeee/roamerserver/RoamerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neeewbeeee.roamerserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoamerServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
